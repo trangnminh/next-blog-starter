5ae4b2b474fc65ff23e0e942b20327dd02853ae1
trangnminh <trangnminh.98@gmail.com>
2023-05-07 18:20:20 +0700

    more tweaks

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container>
         <div className="flex flex-col items-center py-28 lg:flex-row">
           <h3 className="mb-10 text-center text-4xl font-bold leading-tight tracking-tighter lg:mb-0 lg:w-1/2 lg:pr-4 lg:text-left lg:text-[2.5rem]">
-            Statically Generated with Next.js.
+            Statically generated with Next.js.
           </h3>
           <div className="flex flex-col items-center justify-center lg:w-1/2 lg:flex-row lg:pl-4">
             <a
